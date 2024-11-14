f4e7403d6bd061d71d2667c7c30a0df0562a2342
Pangreor <64980093+Pangreor@users.noreply.github.com>
2024-11-14 20:18:09 +0000

    refactor IssueCreationController to WebhookController

--- src/main/kotlin/uk/ac/york/gpig/teamb/aiassistant/controllers/WebhookController.kt
+++ src/main/kotlin/uk/ac/york/gpig/teamb/aiassistant/controllers/WebhookController.kt
@@ -14,42 +14,27 @@ import uk.ac.york.gpig.teamb.aiassistant.utils.types.WebhookPayload
  * The exact event types are configured on GitHub, and currently only include updates to issues.
  * */
 @RestController
-class IssueCreationController( // reafctor name to be EventCreationController (?)
+class WebhookController(
     val issueManager: IssueManager,
 ) {
     val logger = LoggerFactory.getLogger(this::class.java)
 
     @PostMapping("/new-issue")
-    fun receiveNewIssue( // refactor name to be recieveNewEvent (?)
+    fun receiveNewWebhook(
         @RequestHeader("x-github-event") eventType: String,
         @RequestBody body: String,
     ) {
-        when (eventType) {
-            "issues" -> newIssueHandler(body)
-            "issueComment" -> issueCommentHandler(body)
-            else -> logger.info("No handler for event type ${eventType}")
-        }
-        
-    }
-
-    fun newIssueHandler(
-        @RequestBody body: String,
-    ) {
         val issueContents = Gson().fromJson(body, WebhookPayload::class.java)
-        when (issueContents.action) {
-            WebhookPayload.Action.OPENED -> {
+        when (eventType to issueContents.action) {
+            ("issues" to WebhookPayload.Action.OPENED) -> {
                 logger.info("Received new open issue with id ${issueContents.issue.id}")
                 issueManager.processNewIssue(issueContents)
             }
-            else -> {} // Add action types as needed
+            ("issue_comment" to "created") -> {
+                logger.info("Received new comment on issue with id ${issueContents.issue.id}")
+                issueManager.processNewIssueComment(issueContents)
+            }
+            else -> logger.info("No handler for event type $eventType")
         }
-    }
-
-    fun issueCommentHandler(
-        @RequestBody body: String,
-    ) {
-        val issueContents = Gson().fromJson(body, WebhookPayload::class.java)
-        logger.info("Received new comment on issue with id ${issueContents.issue.id}")
-        issueManager.processNewIssueComment(issueContents)
     }
 }
