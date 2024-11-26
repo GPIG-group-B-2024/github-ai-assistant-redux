1375fd7bfbf42e8e66fb8c548bcbfd94a6498d05
dont-text-me <ivanbar3330@gmail.com>
2024-11-26 18:17:19 +0000

    initial C4 manager structure. TODO:  1. Make sure all the right fields are returned when converting from database -> structurizr  2. Add unit tests for write facade  3. Add any missing functionality and tests to the read facade  4. Add tests for the manager

--- src/main/kotlin/uk/ac/york/gpig/teamb/aiassistant/database/C4Manager.kt
+++ src/main/kotlin/uk/ac/york/gpig/teamb/aiassistant/database/C4Manager.kt
@@ -1,14 +1,22 @@
 package uk.ac.york.gpig.teamb.aiassistant.database
 
+import com.structurizr.dsl.StructurizrDslParser
 import org.springframework.stereotype.Service
+import org.springframework.transaction.support.TransactionTemplate
 import uk.ac.york.gpig.teamb.aiassistant.database.entities.C4ElementEntity
+import uk.ac.york.gpig.teamb.aiassistant.database.entities.C4RelationshipEntity
+import uk.ac.york.gpig.teamb.aiassistant.database.entities.C4WorkspaceEntity
 import uk.ac.york.gpig.teamb.aiassistant.database.exceptions.NotFoundException.NotFoundByNameException
 import uk.ac.york.gpig.teamb.aiassistant.database.facades.C4NotationReadFacade
+import uk.ac.york.gpig.teamb.aiassistant.database.facades.C4NotationWriteFacade
+import uk.ac.york.gpig.teamb.aiassistant.enums.MemberType
 import java.util.UUID
 
 @Service
 class C4Manager(
     private val c4NotationReadFacade: C4NotationReadFacade,
+    private val c4NotationWriteFacade: C4NotationWriteFacade,
+    private val transactionTemplate: TransactionTemplate,
 ) {
     fun gitRepoToStructurizrDsl(repoName: String): String {
         val workspace =
@@ -28,6 +36,74 @@ class C4Manager(
             |   $relationshipsBlock
             |}
             """.trimMargin()
+    }
+
+    /**
+     * Consumes a structurizr file representing a single workspace and stores result to database.
+     * If the file is correct, the following entities will be created:
+     *  * A `Member` for each C4 component
+     *  * A `Relationship` for each relationship
+     *  * A `Workspace` entity linked to the provided github repo name
+     * */
+    fun consumeStructurizrWorkspace(
+        repoName: String,
+        rawInput: String,
+    ) {
+        val parser = StructurizrDslParser()
+        parser.parse(rawInput)
+        val workspace = parser.workspace
+        val workspaceId = UUID.randomUUID()
+
+        val workspaceEntity =
+            C4WorkspaceEntity(
+                id = workspaceId,
+                name = workspace.name,
+                description = workspace.description,
+            )
+
+        val rawComponents = workspace.model.elements
+        val rawComponentHierarchy =
+            rawComponents.groupBy {
+                it.parent?.id
+            } // group parsed components by their parent ID (null for top-level components)
+        val stringIdToDbUUID =
+            rawComponents.map {
+                it.id
+            }.associateWith { UUID.randomUUID() } // assign a unique UUID (to be stored in the database) to each discovered component
+        // for each parentId (including null), create entities representing its children and associate them with the parent ID
+        val entities =
+            rawComponentHierarchy.flatMap {
+                    (parentId, children) ->
+                val parentUUID = if (parentId != null) stringIdToDbUUID[parentId]!! else null
+                children.map {
+                    C4ElementEntity(
+                        id = stringIdToDbUUID[it.id]!!,
+                        parentId = parentUUID,
+                        type = MemberType.COMPONENT,
+                        name = it.name,
+                        description = null,
+                        workspaceId = workspaceId,
+                    )
+                }
+            }
+
+        val relationships =
+            workspace.model.relationships.map {
+                C4RelationshipEntity(
+                    from = stringIdToDbUUID[it.sourceId]!!,
+                    to = stringIdToDbUUID[it.destinationId]!!,
+                    fromName = it.source.name,
+                    toName = it.destination.name,
+                    description = it.description,
+                    workspaceId = workspaceId,
+                )
+            }
+
+        transactionTemplate.execute {
+            c4NotationWriteFacade.writeWorkspace(workspaceEntity)
+            c4NotationWriteFacade.writeMemberList(entities)
+            c4NotationWriteFacade.writeRelationshipsList(relationships)
+        }
     }
 
     internal fun C4ElementEntity.printWithChildren(
